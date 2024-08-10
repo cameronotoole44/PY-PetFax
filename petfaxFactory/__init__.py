c0455984b13562821c823e43f8d77e008ae06837
cameronotoole44 <cameronotoole404@gmail.com>
2024-08-10 14:26:42 -0400

    fact page

--- petfaxFactory/__init__.py
+++ petfaxFactory/__init__.py
@@ -3,11 +3,18 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+
     @app.route('/')
-    def hello():
+    def index(): 
         return 'Hello, PetFax!'
-    
-    from . import pet
+
+
+    from . import pet 
     app.register_blueprint(pet.bp)
+
+
+    from . import fact
+    app.register_blueprint(fact.bp)
+
 
     return app
